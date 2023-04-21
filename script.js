63c46656c4d85d3b3e29e7ad490d69250ab1ca8a
Reginaldo Alves <37334200+Reginaldo54@users.noreply.github.com>
2023-04-21 00:01:15 -0300

    atv

---
+++ script.js
@@ -0,0 +1,16 @@
+function validateForm() 
+{
+    var nome        = document.forms["formulario"]["nome"].value;
+    var sobrenome   = document.forms["formulario"]["sobrenome"].value;
+    var telefone    = document.forms["formulario"]["telefone"].value;
+    var email       = document.forms["formulario"]["email"].value;
+    var mensagem    = document.forms["formulario"]["mensagem"].value;
+    var turno       = document.forms["formulario"]["turno"].value;
+    var data        = document.forms["formulario"]["data"].value;
+   
+    
+    if (nome == "" || sobrenome == "" || telefone == "" || email == "" || mensagem == "" || turno == "" || data == "" ) {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+        return false;
+    }
+}
